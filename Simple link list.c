9b21fa82514359283277a16084aefebea25a9324
Anuj309 <anujsingh811256@gmail.com>
2025-05-14 17:59:09 +0530

    Update Simple link list.c

--- Simple link list.c
+++ Simple link list.c
@@ -44,7 +44,7 @@ void Quit() {
     exit(0);
 }
 //Insertion in Simple link list
-// At begining
+// At beginning
 void At_beg(){
 	struct node *new;
 	new =(struct node *)malloc(sizeof(struct node));
@@ -52,7 +52,7 @@ void At_beg(){
 	new ->next =NULL;
 	new ->next =start;
 	start = new;
-	
+	printf("New node is inserted at beginning");
 }
 //At End 
 void At_End(){
@@ -65,39 +65,113 @@ void At_End(){
 		temp = temp -> next;
 	}
 	temp ->  next =new;
+	temp=temp->next;
+	printf("New node inserted at end");
 }
+//At Between 
+void At_btw(){
+	struct node *new;
+	struct node *q;
+	int pos;
+	new = (struct node *)malloc(sizeof(struct node));
+	printf("Enter the value : ");
+	scanf("%d",&new ->data);
+	printf("Entyer the position");
+	scanf("%d",&pos);
+    q=start;
+    for(int i=1;i<pos-1;i++){
+    	q=q->next;
+	}
+	new->next=q->next;
+	q->next=new;
+	printf("new node inserted at position %d",pos);
+}
+// Deletion from link list 
+// Deletion at beginning
+void del_At_Beg(){
+	struct node *temp;
+	temp = start;
+	start = temp -> next;
+	printf("deleted node is Done ");
 
+	free(temp);
+}
+//Deletion at End
+void del_At_End(){
+    struct node *q;
+    struct node *temp;
+    temp =start;
+    while(temp ->next !=NULL){
+        q=temp;
+        temp=temp->next;
+    }
+    printf("deleted node is %d", q->next->data);
+    q->next =NULL;
+    free(temp);
+}
+//Deletion At any position
+void del_At_Btw(){
+    struct node *q;
+    struct node *p;
+    int btw;
+    printf("Enter the position :");
+    scanf("%d",&btw);
+    q = start;
+    for(int i =1;i<btw -1;i++){
+        q=q->next;
+    }
+    p=q->next;
+    q->next= p->next;
+    printf("deleted node is %d",p->data);
+    free(p);
+}
 int main(){
  	int choice;
  	    
  	    while(1){
  	    	printf("\n1.Create\n");
  	    	printf("2.Display\n");
-		    printf("3.Insert at Begining\n");
+		    printf("3.Insert at Beginning\n");
 		    printf("4.Insert at End\n");
-		    printf("5.Quit\n");
+		    printf("5.Insert at Between\n");
+		    printf("6.Delete at Beginning\n");
+		    printf("7.Delete at End\n");
+		    printf("8.Delete at Any Position\n");
+		    printf("9.Quit\n");
 		    printf("Enter choice :\n");
 		    scanf("%d",&choice);
 		        
 		        
 		        switch(choice){
-		        	        case 1:
-		        		Create();
-		        		break;
-		        	        case 2:
+		        	       case 1:
+		        		 Create();
+		        		 break;
+		        	       case 2:
 					    Display();	
 					    break;
 					case 3:
                                             At_beg();
 						break;
-					case 4:
+				        case 4:
 					    At_End();
-						break;	
+						break;
 					case 5:
+					    At_btw();
+						break;	
+					case 6:
+					    del_At_Beg();
+						break;
+					 case 7:
+					    del_At_End();
+						break;
+				         case 8:
+					    del_At_Btw();
+						break;
+					  case 9:
 					    Quit();
 						break;	
 					default:
-					   printf("Choice is invalid. Try Again./n");	    
+					   printf("Choice is invalid. Try Again.\n");	    
 				            }
 	     	 } 
 			  return 0;	 
